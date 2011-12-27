7fa2630909e2e008bd47a5e4e369476f53bc6c68
Jeff Miller <jeffmiller@sormula.org>
2011-12-27 17:14:10 -0500

    misc clean up

--- src/main/java/org/sormula/operation/monitor/package-info.java
+++ src/main/java/org/sormula/operation/monitor/package-info.java
@@ -1,7 +1,6 @@
 /**
  * Classes for recording and reporting execution times. These classes are used
- * when {@link org.sormula.Database#setTimings(boolean)} or
- * {@link org.sormula.operation.SqlOperation#setTimings(boolean)} are invoked
- * with true parameter. 
+ * when you enable timings with {@link org.sormula.Database#setTimings(boolean)} or
+ * {@link org.sormula.operation.SqlOperation#setTimings(boolean)}. 
  */
 package org.sormula.operation.monitor;

bc4187ecb8839d3b6c6c5a44dcb34bc7efcbb2d4
Jeff Miller <jeffmiller@sormula.org>
2012-01-20 11:09:58 -0500

    update Copyright year to 2012

--- src/main/java/org/sormula/translator/RowTranslator.java
+++ src/main/java/org/sormula/translator/RowTranslator.java
@@ -1,5 +1,5 @@
 /* sormula - Simple object relational mapping
- * Copyright (C) 2011 Jeff Miller
+ * Copyright (C) 2011-2012 Jeff Miller
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /* sormula - Simple object relational mapping
- * Copyright (C) 2011 Jeff Miller
+ * Copyright (C) 2011-2012 Jeff Miller
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/tests/java/org/sormula/tests/operation/SaveTest.java
+++ src/tests/java/org/sormula/tests/operation/SaveTest.java
@@ -1,5 +1,5 @@
 /* sormula - Simple object relational mapping
- * Copyright (C) 2011 Jeff Miller
+ * Copyright (C) 2011-2012 Jeff Miller
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

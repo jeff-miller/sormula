52bc109a5c8054edc8b7a588ddda747ea44cb60e
Jeff Miller <jeffmiller@sormula.org>
2015-03-06 09:25:57 -0500

    update comments

--- src/main/java/org/sormula/cache/readonly/ReadOnlyCache.java
+++ src/main/java/org/sormula/cache/readonly/ReadOnlyCache.java
@@ -36,11 +36,11 @@ import org.sormula.operation.SqlOperation;
 
 /**
  * A cache that retains rows that selected, inserted, updated, and deleted. Rows are never written to the 
- * database. The selected and modified rows are retained in cache for life of {@link Table} object or until 
+ * database by the cache. The traditional sormula modify operations perform the updates immediately without
+ * caching. The selected and modified rows are retained in cache for life of {@link Table} object or until 
  * explicitly flushed with {@link Table#flush()}, or {@link Database#flush()} or evicted with
  * {@link Cache#evict(Object)} or {@link Cache#evictAll()}.
  * <p>
- * TODO can ReadOnlyCache be modified so that transaction is optional?
  * This is a transactional cache which means that caching is performed relative to database transactional
  * boundaries of begin, commit, and rollback. Tables that are cached may not read/write unless a transaction 
  * is active. Tables that are cached must use {@link Transaction} obtained from {@link Database#getTransaction()}
@@ -71,8 +71,8 @@ public class ReadOnlyCache<R> extends AbstractCache<R>
     {
         super(table, cachedAnnotation);
     }
-    
-    
+
+
     /**
      * Prepares cache for use.
      * 

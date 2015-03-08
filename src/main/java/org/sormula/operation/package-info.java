c34fea5adddba1254eb4103093849a38afd71dbb
Jeff Miller <jeffmiller@sormula.org>
2015-03-08 13:33:33 -0400

    update documentation

--- src/main/java/org/sormula/operation/package-info.java
+++ src/main/java/org/sormula/operation/package-info.java
@@ -17,20 +17,45 @@
 
 
 /**
- * Classes that perform SQL operations such as select, update, insert, delete.
+ * Classes that perform SQL operations such as select, update, insert, save, and delete.
  * <p> 
- * There are three types of operations: select operations, and modify operations, 
+ * There are two types of operations: select operations and modify operations. 
  * All select operations are derived from 
- * {@link org.sormula.operation.SelectOperation}. Insert, update, and, delete 
+ * {@link org.sormula.operation.SelectOperation}. Insert, save, update, and, delete 
  * operations are derived from {@link org.sormula.operation.ModifyOperation}. 
- * For example:
  * <p>
- * Select all students by type 3 ("byType" is name of Where annotation on Student):
+ * Use operation classes instead of {@link org.sormula.Table} methods if you
+ * need to perform the same operation many times but only want to prepare it once
+ * for efficiency. 
+ * <p>
+ * For example, select all students by type 3 and then by type 4 with the same operation.
+ * "byType" is name of Where annotation on Student:
  * <blockquote><pre>
  * Database database = ...
  * Table&lt;Student&gt; table = database.getTable(Student.class);
- * List&lt;Student&gt; selectedList = new ArrayListSelect&lt;Student&gt;(table, "byType").selectAll(3);
+ * ArrayListSelectOperation&lt;Student&gt op = 
+ *     new ArrayListSelectOperation&lt;&gt;(table, "byType");
+ * op.setParameters(3);
+ * op.execute(); // JDBC prepare occurs here
+ * List&lt;Student&gt; selectedList3 = op.readAll();    
+ * op.setParameters(4);
+ * op.execute(); // no prepare needed
+ * List&lt;Student&gt; selectedList4 = op.readAll();
+ * op.close();
  * </pre></blockquote>
+ * To update rows (insert, save, and delete are similar):
+ * <blockquote><pre>
+ * Database database = ...
+ * Table&lt;Student&gt; table = database.getTable(Student.class);
+ * UpdateOperation&lt;Student&gt op = new UpdateOperation&lt;&gt;(table);
+ * List&lt;Student&gt; list1 = ...;    
+ * op.setRows(list1);
+ * op.execute(); // JDBC prepare occurs here
+ * List&lt;Student&gt; list2 = ...; 
+ * op.setRows(list2);
+ * op.execute(); // no prepare needed
+ * op.close();
+ * </pre></blockquote> 
  */
 package org.sormula.operation;
 
